97b0e9ed9f596ba1d5a3b12e4d30b913e2da8ce7
Anish Kumar <anishbishnoi127@gmail.com>
2023-06-04 12:52:24 +0530

    components called as a function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,22 @@ export default function Home() {
     return <main>{"I'm Admin"}</main>;
   };
 
+  const callMe = () => {
+    const DisplayMsg = () => {
+      return (
+        <main>
+          <h1>Components Called as a Function 😁 </h1>
+        </main>
+      );
+    };
+    return <DisplayMsg />;
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Who is Logged In ?</h1>
       <button onClick={() => setIsTrue(!isTrue)}>Switch User</button>
       {!isTrue ? <User /> : <Admin />}
+      {isTrue ? callMe() : " "}
     </main>
   );
 }
